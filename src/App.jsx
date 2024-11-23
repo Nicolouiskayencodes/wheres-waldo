7aa47e71ef8da7810d1a018ee2f68a15e339ba35
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-11-22 20:50:51 -0800

    Make coordinates and select box scale with size of image

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,50 @@
 import avengers from './assets/avengers.jpg'
+import selectBoxSVG from './assets/selectBox.svg'
+import { useRef, useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
+  const imageref = useRef(null)
+  const [imagewidth, setImageWidth] = useState(null);
+
+  useEffect(() => {
+    setTimeout(()=>{
+     getWidth();}
+    , 0
+    )
+    function getWidth() {
+      setImageWidth(imageref.current.getBoundingClientRect().width/1280)
+    }
+    window.addEventListener('resize', getWidth);
+    addEventListener('load', getWidth)
+    return window.removeEventListener('resize', getWidth);
+  }, [])
+
+  document.addEventListener('click', (event)=>{
+    const imagebox = document.querySelector('#imagebox')
+    const selectbox = document.querySelector('#selectbox')
+    if (!(event.target === imagebox)){
+      selectbox.style.display = 'none'
+    }
+  })
+  
   function captureCoordinates(event) {
     const imagebox = document.querySelector('#imagebox')
+    const selectbox = document.querySelector('#selectbox')
     const imageOffset = imagebox.getBoundingClientRect()
-    let x = 0
-    let y = 0
-    if (window.matchMedia("(max-width: 650px)").matches){
-      x =(event.pageX-imageOffset.left)*1280/400
-      y =(event.pageY-imageOffset.top)*1280/400
-    } else if (window.matchMedia("(max-width: 1050px)").matches){
-      x =(event.pageX-imageOffset.left)*1280/600
-      y =(event.pageY-imageOffset.top)*1280/600
-    } else if (window.matchMedia("(max-width: 1300px)").matches){
-      x =(event.pageX-imageOffset.left)*1280/1000
-      y =(event.pageY-imageOffset.top)*1280/1000
-    } else {
-      x =(event.pageX-imageOffset.left)
-      y =(event.pageY-imageOffset.top)
-    }
-   return console.log(x,y)
+    const x = Math.round((event.pageX-imageOffset.left)*1280/imagebox.width);
+    const y = Math.round((event.pageY-imageOffset.top)*853/imagebox.height);
+    console.log(x,y)
+    selectbox.style.left = event.pageX-(selectbox.width/2) + 'px'
+    selectbox.style.top = event.pageY-(selectbox.height/2) + 'px'
+    selectbox.style.display = 'block'
   }
 
   return (
     <>
-      <button onClick={captureCoordinates}>
-        <img id='imagebox' src={avengers}></img>
+      <button id='imagebutton' onClick={captureCoordinates}>
+        <img src={selectBoxSVG} id='selectbox' style={{transform: `scale(${imagewidth})`}}></img>
+        <img id='imagebox' src={avengers} ref={imageref}></img>
       </button>
     </>
   )
