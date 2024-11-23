443d4cdea59b6a0d8ee32296ef63449bd48920fd
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-11-22 17:53:54 -0800

    Capture relative x,y coordinates of photo in different media sizes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import avengers from './assets/avengers.jpg'
-
 import './App.css'
 
 function App() {
+  function captureCoordinates(event) {
+    const imagebox = document.querySelector('#imagebox')
+    const imageOffset = imagebox.getBoundingClientRect()
+    let x = 0
+    let y = 0
+    if (window.matchMedia("(max-width: 650px)").matches){
+      x =(event.pageX-imageOffset.left)*1280/400
+      y =(event.pageY-imageOffset.top)*1280/400
+    } else if (window.matchMedia("(max-width: 1050px)").matches){
+      x =(event.pageX-imageOffset.left)*1280/600
+      y =(event.pageY-imageOffset.top)*1280/600
+    } else if (window.matchMedia("(max-width: 1300px)").matches){
+      x =(event.pageX-imageOffset.left)*1280/1000
+      y =(event.pageY-imageOffset.top)*1280/1000
+    } else {
+      x =(event.pageX-imageOffset.left)
+      y =(event.pageY-imageOffset.top)
+    }
+   return console.log(x,y)
+  }
 
   return (
     <>
-      <button>
-        <img className='imagebox' src={avengers}></img>
+      <button onClick={captureCoordinates}>
+        <img id='imagebox' src={avengers}></img>
       </button>
     </>
   )
